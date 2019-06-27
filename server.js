39429cc04b0cb1d6e890452dd11792276a00879c
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-27 18:36:39 -0400

    Added knexfile

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const server = express();
 
+const knex = require('knex');
+
+const knexConfig = {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/users-data.db3'
+    },
+    useNullAsDefault: true,
+}
+
 const db = require('./data/users-auth');
 const bcrypt = require('bcryptjs');
 
 server.use(express.json());
+
+
 
 //Sanity Check
 server.get('/', (req, res) => {
