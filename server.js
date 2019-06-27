d64452a69b8271e2c48c83ee4ca76c0fc568f3a2
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-06-27 18:30:46 -0400

    Adding knex

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
-
-const db = require('./data/user');
-
 const server = express();
 
+const db = require('./data/users-auth');
 const bcrypt = require('bcryptjs');
 
 server.use(express.json());
 
+//Sanity Check
 server.get('/', (req, res) => {
-  res.send("Server is working!");
+    res.send("Server is working!");
 });
 
 
